fa97a25caee9464fada093c714d5855a63b335fa
outpost <krisnooklyn@gmail.com>
2022-02-17 17:14:08 -0600

    add user spec add user helper add common helper add config helper

--- config/setup.js
+++ config/setup.js
@@ -1,9 +1,8 @@
 import 'dotenv/config'
 import AuthHelper from '../helpers/auth.helper'
 
-before(async function(){
+before(async function() {
     const authHelper = new AuthHelper()
-    await authHelper.login(process.env.LOGIN,process.env.PASSWORD)
-    process.env["TOKEN"] = authHelper.response.body.token
+    await authHelper.login(process.env.LOGIN, process.env.PASSWORD)
+    process.env['TOKEN'] = authHelper.response.body.token
 })
-
